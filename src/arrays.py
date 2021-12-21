0c469083239956d55a4286ac2ef3dc8ef1c0f98a
Juan Formoso <trajano.juan@gmail.com>
2021-12-21 00:42:17 -0400

    Add array file

---
+++ src/arrays.py
@@ -0,0 +1,17 @@
+# Coleções(listas)
+preco_1 = 10
+preco_2 = 20
+preco_3 = 30
+# Lista
+precos = [10, 20, 30]
+# 0, 1, 2
+print(precos.index[0])
+print(precos.index(200))
+# Lista
+diversidades = [15, 'Johnatan', True]
+print(diversidades[0])
+print(diversidades[1])
+print(diversidades[2])
+# Laços em iteráveis.
+for preco in precos:
+  print(preco)
