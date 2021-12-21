6b43ab39e5d458891d28cb71005f52584cb61c4c
Juan Formoso <trajano.juan@gmail.com>
2021-12-20 20:53:32 -0400

    Change file name and start code

--- src/variables.py
+++ src/variables.py
@@ -1,1 +1,23 @@
-# Variáveis
+# Variaveis
+velocidade_internet = 10
+print(velocidade_internet)
+nota_filme = 8.5 # float
+# Numeros
+# Valores booleanos
+esta_aberto = False
+# Strings
+nome_do_curso = "Logica de Programacao"
+
+# Como variaveis seriam usadas em um programa real?
+# Problema 1 - Valor por Hora
+# Escreva um programa que retorna o valor de um funcionario com base no seu salário mensal e horas trabalhadas por mes.
+'''
+input salario_mensal
+input horas_trabalhadas
+valor_hora = salario_mensal /horas_trabalhadas
+print valor_hora
+'''
+salario_mensal = input('Digite o salário mensal: ')
+horas_trabalhadas = input('Digite as horas trabalhadas: ')
+valor_hora = int(salario_mensal) / int(horas_trabalhadas)
+print(valor_hora)
